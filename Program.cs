221d21ad10f08b8f75e6a742576b4ffe760b3416
Jacob Kohl <jkohl7@my.wctc.edu>
2020-09-27 22:54:09 -0500

    Complete

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+
+namespace SleepData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         // ask for input
+            Console.WriteLine("Enter 1 to create data file.");
+            Console.WriteLine("Enter 2 to parse data.");
+            Console.WriteLine("Enter anything else to quit.");
+            // input response
+            string resp = Console.ReadLine();
+
+            if (resp == "1")
+            {
+                // create data file
+
+                 // ask a question
+                Console.WriteLine("How many weeks of data is needed?");
+                // input the response (convert to int)
+                int weeks = int.Parse(Console.ReadLine());
+                 // determine start and end date
+                DateTime today = DateTime.Now;
+                // we want full weeks sunday - saturday
+                DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+                // subtract # of weeks from endDate to get startDate
+                DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+                Console.WriteLine(dataDate);
+                
+                // random number generator
+                Random rnd = new Random();
+
+                // create file
+                StreamWriter sw = new StreamWriter("data.txt");
+
+                // loop for the desired # of weeks
+                while (dataDate < dataEndDate)
+                {
+                    // 7 days in a week
+                    int[] hours = new int[7];
+                    for (int i = 0; i < hours.Length; i++)
+                    {
+                        // generate random number of hours slept between 4-12 (inclusive)
+                        hours[i] = rnd.Next(4, 13);
+                    }
+                    // M/d/yyyy,#|#|#|#|#|#|#
+                    Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+                    // add 1 week to date
+                    dataDate = dataDate.AddDays(7);
+                }
+                sw.Close();
+            }
+             else if (resp == "2")
+            {
+                // parse data file
+                if (File.Exists("data.txt"))
+                {
+                    // read data from file
+                    StreamReader sr = new StreamReader("data.txt");
+                    while (!sr.EndOfStream)
+                    {
+                        //Read and split the data
+                        string line = sr.ReadLine();
+                        string[] week = line.Split(',');
+                        
+                        //Start the first week of data
+                        DateTime date = DateTime.Parse(week[0]);
+                        int[] hours = Array.ConvertAll(week[1].Split('|'), int.Parse);
+
+                        //Format for the date and space out data
+                        Console.WriteLine($"Week of {date:MM}, {date:dd}, {date:yyyy}");
+                        Console.WriteLine($"{"Su",3}{"Mo",3}{"Tu",3}{"We",3}{"Th",3}{"Fr",3}{"Sa",3}");
+                        Console.WriteLine($"{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}{"--",3}"); 
+
+                        // display the array
+                        Console.WriteLine($"{hours[0],3}{hours[1],3}{hours[2],3}{hours[3],3}{hours[4],3}{hours[5],3}{hours[6],3}");
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                     Console.WriteLine("File does not exist");
+                }
+            }
+        }
+    }
+}
